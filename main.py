0b18953de877054345d60cfd2528d5d691f2dd80
vilejaninov <vilvm88@gmail.com>
2019-03-19 10:41:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import shutil
 import argparse
 
-directories = [
+includes = [
     r"\TestsBin",
     r"\obj",
     r"\bin",
@@ -13,6 +13,12 @@ directories = [
     r"\Deployment"
 ]
 
+excludes = [
+    r".git",
+    r"\Deployment\Release_1_0",
+    r"\Deployment\ADM_1824"
+]
+
 
 def get_arguments():
     parser = argparse.ArgumentParser()
@@ -20,14 +26,49 @@ def get_arguments():
     return parser.parse_args()
 
 
+def get_directories_for_clean(path):
+    directories = []
+
+    for dirname, dirnames, filenames in os.walk(path):
+        if not os.listdir(dirname):
+            directories.append(dirname)
+            continue
+
+        for dir in excludes:
+            if dirname.find(dir) >= 0 and dir in directories:
+                directories.remove(dir)
+                break
+
+        for dir in includes:
+            if dirname.endswith(dir):
+                directories.append(dirname)
+
+    return directories
+
+
+def clean(path):
+
+    directories = get_directories_for_clean(path)
+
+    if len(directories) == 0:
+        return 0
+
+    count = 0
+    for dir in directories:
+        if not os.path.isdir(dir):
+            continue
+        shutil.rmtree(dir)
+        print(dir, "removed")
+        count += 1
+    return count
+
+
 def main():
     args = get_arguments()
 
-    for dirname, dirnames, filenames in os.walk(args.directory):
-        for dir in directories:
-            if dirname.endswith(dir) and dirname.find(".git") == -1:
-                shutil.rmtree(dirname)
-                print(dirname, "removed")
+    while True:
+        if clean(args.directory) == 0:
+            break
 
 
 if __name__ == "__main__":
